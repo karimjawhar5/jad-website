23afed5aed1b95285b33a8fee214a0ae73a03a0f
Karim Jaouhar <Karimjawhar5@gmail.com>
2023-04-27 02:51:08 -0400

    Added Experience timline

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { TimeLine } from './timeLine/TimeLine'
 
 function Experience() {
   return (
     <div>
-      Experience
+      <h2 className='mb-6 font-medium'>Experience</h2>
+      <TimeLine />
     </div>
   )
 }
